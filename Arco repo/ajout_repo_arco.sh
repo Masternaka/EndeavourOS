f88c01212868c1e7ab6115756687b7557e45f674
Masternaka <gabthesniper@gmail.com>
2024-10-09 11:41:37 -0400

    Create ajout_repo_arco.sh

---
+++ Arco repo/ajout_repo_arco.sh
@@ -0,0 +1,9 @@
+À partir du terminal, éxécuter la commande suivante:
+
+git clone https://github.com/arcolinux/arcolinux-spices
+
+Navigate inside the arcolinux-spices folder and go to the …/usr/share/arcolinux-spices/scripts folder.
+
+Run the script
+
+sudo ./get-the-keys-and-repos.sh
