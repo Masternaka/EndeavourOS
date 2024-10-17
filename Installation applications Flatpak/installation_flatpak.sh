ac66127063f9026472a6ff2bb2b86c64001b8a58
Masternaka <gabthesniper@gmail.com>
2024-10-17 11:49:55 -0400

    Add files via upload

--- Installation applications Flatpak/installation_flatpak.sh
+++ Installation applications Flatpak/installation_flatpak.sh
@@ -1,1 +1,23 @@
+#!/bin/bash
 
+# Vérifie si flatpak est installé
+if ! command -v flatpak &> /dev/null
+then
+    echo "Flatpak n'est pas installé. Veuillez l'installer d'abord."
+    exit 1
+fi
+
+# Liste des applications Flatpak à installer
+applications=(
+  
+
+  
+)
+
+# Installation des applications
+for app in "${applications[@]}"; do
+    echo "Installation de $app ..."
+    flatpak install -y flathub $app
+done
+
+echo "Installation terminée."
